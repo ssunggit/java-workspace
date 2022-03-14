7390da5ff7fbdeaf2b85c456c52cd97435f5b39b
ssunggit <gustjd68@gmail.com>
2022-03-14 17:53:21 +0900

    commit

---
+++ JAVA/src/kr/ac/kopo/homework/day3/Solution01.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.homework.day3;
+
+import java.util.Scanner;
+
+public class Solution01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
