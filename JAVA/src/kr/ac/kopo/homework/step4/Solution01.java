9abdf2bf6c2e20662e431eec771be5e9c0c4f5df
ssunggit <gustjd68@gmail.com>
2022-03-15 19:38:11 +0900

    homework

--- JAVA/src/kr/ac/kopo/homework/step4/Solution01.java
+++ JAVA/src/kr/ac/kopo/homework/step4/Solution01.java
@@ -25,6 +25,48 @@ package kr.ac.kopo.homework.step4;
    
    
  */
-public class Solution01 {
 
+import java.util.Scanner;
+
+class IceCream{
+	String name;
+	int price;
+}
+
+public class Solution01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("아이스크림 몇개 구입할꺼야? => ");
+		int count = sc.nextInt();
+		
+		IceCream[] icArr=new IceCream[count];	
+		
+		int sum = 0;
+		sc.nextLine();
+		
+		for(int i = 0; i < count ; i++) {
+			System.out.printf("%d번째 아이스크림 구매정보\n", i+1);
+			IceCream ic = new IceCream();
+			
+			System.out.print("아이스크림명 : ");
+			ic.name = sc.nextLine();
+			
+			System.out.print("아이스크림가격 : ");
+			ic.price = sc.nextInt();
+				
+			sum += ic.price;
+			sc.nextLine();
+			icArr[i] = ic;
+		}
+		
+		
+		for(IceCream ic : icArr) {
+			System.out.println(ic.name);
+			System.out.println(ic.price);
+		}
+		
+		System.out.println(sum);
+		
+	}
+	
 }
