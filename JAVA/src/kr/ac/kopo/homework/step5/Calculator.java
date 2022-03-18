235c5e588d1628ad77e5296c849fbfa3272a35c0
ssunggit <gustjd68@gmail.com>
2022-03-18 18:14:57 +0900

    commit

---
+++ JAVA/src/kr/ac/kopo/homework/step5/Calculator.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.homework.step5;
+/*
+ * 
+ 정수 : 12
+정수 : 5
+12 + 5 = 17
+12 - 5 = 7
+12 * 5 = 60
+12 / 5 = 2.4
+12 소수체크 : false
+5 소수체크 : true
+ 
+ */
+public class Calculator {
+
+}
---
+++ JAVA/src/kr/ac/kopo/homework/step5/StringUtil.java
@@ -0,0 +1,29 @@
+package kr.ac.kopo.homework.step5;
+/*
+*1. 하나의 문자를 입력받아 해당 문자가 대문자이면 true 를 반환하는 isUpperChar( char c ) 메소드를 작성하시오. 
+*2. 하나의 문자를 입력받아 해당 문자가 소문자이면 true를 반환하는 isLowerChar(char c) 메소드를 작성하시오.
+*3. 두개의 숫자를 입력받아 큰수를 반환하는 max( int i, int j) 메소드를 작성하시오.
+*4. 두개의 숫자를 입력받아 작은수를 반환하는 min(int i, int j) 메소드를 작성하시오.
+*5. 문자열을 입력받아 거꾸로 변경하는 reverseString(String str) 메소드를 작성하시오.
+*6. 문자열을 입력받아 대문자로 변경하는 toUpperString(String str) 메소드를 작성하시오.
+*7. 문자열을 입력받아 소문자로 변경하는 toLowerString(String str) 메소드를 작성하시오.
+ */
+public class StringUtil {
+	// 1. 하나의 문자를 입력받아 해당 문자가 대문자이면 true 를 반환하는 isUpperChar( char c ) 메소드를 작성하시오. 
+	boolean isUpperChar(char c) {
+		
+		return true;
+	}
+	
+	// 2. 하나의 문자를 입력받아 해당 문자가 소문자이면 true를 반환하는 isLowerChar(char c) 메소드를 작성하시오.
+	boolean isLowerChar(char c) {
+	
+		return true;
+	}
+	
+	// 3. 
+	
+	
+	
+	
+}
