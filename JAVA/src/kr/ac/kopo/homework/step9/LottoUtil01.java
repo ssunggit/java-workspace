9ea091c637811125a0ee94f9dcae976e06627249
ssunggit <gustjd68@gmail.com>
2022-03-25 16:49:27 +0900

    0325 homework form

--- JAVA/src/kr/ac/kopo/homework/step9/LottoUtil01.java
+++ JAVA/src/kr/ac/kopo/homework/step9/LottoUtil01.java
@@ -1,46 +1,16 @@
 package kr.ac.kopo.homework.step9;
-// 에외처리도 해도됨
-
-// 번호추출하는 방법을 5가지 이상
-// getNumbers 메소드 5개
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- 로또 게임
-게임수를 입력하세요 : 3
-게임 1 : [2, 6, 13, 25, 20, 7]
-게임 2 : [2, 9, 3, 7, 38, 13]
-게임 3 : [40, 10, 38, 39, 42, 18]
 
 
-class LottoUtil {
-
-    public void start() {
-	게임수 입력
-        for(int i = 0; i < cnt; i++) {
- 
-	   게임1 : Arrays.toString(getNumbers())
-        }
-
-    }
-
-    private int[] getNumbers() {
-
-	// 1 ~ 45 사이의 로또번호 6개 추출
-    }
-
-
-}
-
-
- */
-
-public class LottoUtil {
+// 배열 + for문이용
+public class LottoUtil01 {
 	public static void main(String[] args) {
-		LottoUtil l = new LottoUtil();
+		System.out.println("로또게임");
+		LottoUtil01 l = new LottoUtil01();
 		l.start();
 	}
 
---
+++ JAVA/src/kr/ac/kopo/homework/step9/LottoUtil05.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.homework.step9;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+
+public class LottoUtil05 {
+	public static void main(String[] args) {
+		LottoUtil05 l = new LottoUtil05();
+		l.start();
+	}
+
+	public void start() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("게임수를 입력하세요 : ");
+		int cnt = sc.nextInt();
+
+		for (int i = 0; i < cnt; i++) {
+			System.out.print("게임 " + (i + 1) + " : ");
+			System.out.println(Arrays.toString(getNumbers()));
+		}
+
+	}
+
+	private int[] getNumbers() {
+		return null;
+	}
+
+}
