c2d8d1686763a48f349c679f08e819a81b8af8be
ssunggit <gustjd68@gmail.com>
2022-03-25 16:42:14 +0900

    0325 homework1

--- JAVA/src/kr/ac/kopo/homework/step9/LottoUtil.java
+++ JAVA/src/kr/ac/kopo/homework/step9/LottoUtil.java
@@ -1,7 +1,12 @@
 package kr.ac.kopo.homework.step9;
 // 에외처리도 해도됨
+
 // 번호추출하는 방법을 5가지 이상
 // getNumbers 메소드 5개
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 /*
  로또 게임
@@ -33,8 +38,40 @@ class LottoUtil {
 
  */
 
-
 public class LottoUtil {
-	
-	
+	public static void main(String[] args) {
+		LottoUtil l = new LottoUtil();
+		l.start();
+	}
+
+	public void start() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("게임수를 입력하세요 : ");
+		int cnt = sc.nextInt();
+
+		for (int i = 0; i < cnt; i++) {
+			System.out.print("게임 " + (i + 1) + " : ");
+			System.out.println(Arrays.toString(getNumbers()));
+		}
+
+	}
+
+	private int[] getNumbers() {
+		int[] arr = new int[6];
+		// 1 ~ 45 사이의 로또번호 6개 추출(중복허용하지 않는)
+		while (arr[5] == 0) {
+			Random r = new Random();
+			arr[0] = r.nextInt(45) + 1;
+			
+			for (int i = 0; i < 6; i++) {
+				int num = r.nextInt(45) + 1;
+				if (!(arr[i] == num)) {
+					arr[i] = num;
+				}
+			}
+		}
+		return arr;
+
+	}
+
 }
