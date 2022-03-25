d9d387dbe257f72df0d2278784c16e302f27e079
ssunggit <gustjd68@gmail.com>
2022-03-25 15:29:34 +0900

    0325 commit

--- JAVA/src/kr/ac/kopo/day12/WrapperMain.java
+++ JAVA/src/kr/ac/kopo/day12/WrapperMain.java
@@ -2,7 +2,7 @@ package kr.ac.kopo.day12;
 
 /*
  * 
- * Wrapper Class : 기본자료형의 맴버변수를 참조자료형으로 갖게하는 클래스
+ * Wrapper Class : 기본자료형의 변수를 참조자료형으로 갖게하는 클래스
  * 
  * 
 	기본자료형			Wrapper Class
@@ -24,6 +24,7 @@ public class WrapperMain {
 	public static void main(String[] args) {
 			
 			int i = 100;
+							// Wrapper Class를 객체화시킬 필요가 없음
 			Integer i2 = new Integer(100);
 			Integer i5 = Integer.valueOf(100);
 			Integer i3 = 100;	// auto boxing
@@ -31,7 +32,7 @@ public class WrapperMain {
 			
 								// 숫자스트링을 정수로 바꿈
 			System.out.println(Integer.parseInt("123") + 100);
-			
+								// 옛날에는 아래 코드가 정수화가 안됨-> 오토언박싱의 등장으로 가능
 			System.out.println(Integer.valueOf("123") + 100);
 	}
 }
